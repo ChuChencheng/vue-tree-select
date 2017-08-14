a6553e8dc44fbb5126b49f86a6aa1d7e3a319ff2
chucc <chucc@wangsu.com>
2017-08-14 19:28:16 +0800

    实现选中

--- src/components/Item.vue
+++ src/components/Item.vue
@@ -1,6 +1,6 @@
 <template>
   <li>
-    <div class="vts-option" @click="handleSelect">
+    <div class="vts-option" :class="{ 'vts-selected': selected }" @click="handleSelect">
       <span v-for="n in getLayer - 1">&nbsp;</span>
       <span class="vts-expander" v-if="isFolder" @click.stop="handleExpand">{{ open ? '-' : '+' }}</span>
       <span :class="icon"></span>
@@ -8,81 +8,98 @@
     </div>
     <ul v-if="isFolder" v-show="open">
       <item
-        v-for="(children, index) in tree.children"
-        :key="index"
-        :tree="children"
-        :level="`${level}.${index}`">
-      </item>
-    </ul>
-  </li>
+      v-for="(children, index) in tree.children"
+      :key="index"
+      :tree="children"
+      :level="`${level}.${index}`">
+    </item>
+  </ul>
+</li>
 </template>
 
 <script>
-import EventBus from './EventBus';
+  import EventBus from './EventBus';
 
-export default {
-  name: 'item',
-  props: {
-    tree: {
-      type: Object,
-      required: true,
+  export default {
+    name: 'item',
+    props: {
+      tree: {
+        type: Object,
+        required: true,
+      },
+      selectLeafOnly: {
+        type: Boolean,
+        default: true,
+      },
+      level: {
+        type: String,
+        default: '0',
+      },
+      icon: String,
     },
-    selectLeafOnly: {
-      type: Boolean,
-      default: true,
+    created() {
+      this.attachEvents();
     },
-    level: {
-      type: String,
-      default: '0',
-    },
-    icon: String,
-  },
-  data() {
-    return {
-      open: true,
-    };
-  },
-  methods: {
-    handleExpand() {
-      this.open = !this.open;
-    },
-    handleSelect() {
-      const opt = {
-        name: this.tree.name,
-        value: this.tree.value,
-        level: this.value,
+    data() {
+      return {
+        open: true,
+        selected: false,
       };
-      EventBus.$emit('select', opt);
     },
-  },
-  computed: {
-    isFolder() {
-      return this.tree.children && this.tree.children.length;
+    methods: {
+      attachEvents() {
+        EventBus.$on('select', this.handleOtherSelect);
+      },
+      handleExpand() {
+        this.open = !this.open;
+      },
+      handleSelect() {
+        const opt = {
+          name: this.tree.name,
+          value: this.tree.value,
+          level: this.level,
+        };
+        EventBus.$emit('select', opt);
+        this.selected = true;
+      },
+      handleOtherSelect(opt) {
+        if (opt.value !== this.tree.value) {
+          this.selected = false;
+        }
+      },
     },
-    getLayer() {
-      return this.isFolder ? this.level.split('.').length : this.level.split('.').length + 1;
+    computed: {
+      isFolder() {
+        return this.tree.children && this.tree.children.length;
+      },
+      getLayer() {
+        return this.isFolder ? this.level.split('.').length : this.level.split('.').length + 1;
+      },
     },
-  },
-};
+  };
 </script>
 
 <style lang="less" scoped>
+  * {
+    margin: 0;
+    padding: 0;
+  }
   ul {
     list-style-type: none;
     padding-left: 0;
   }
   .vts-option {
+    height: 20px;
     cursor: default;
     user-select: none;
     &:hover{
       background-color: #ade4ec;
     }
-    & span {
-      display: inline-block;
-      width: 5px;
-    }
   }
   .vts-expander {
     cursor: pointer;
+  }
+  .vts-selected {
+    background-color: #ade4ec;
   }
 </style>
--- src/components/TreeSelect.vue
+++ src/components/TreeSelect.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="vts-container">
     <input class="js-vts-input" type="text" :name="name" readonly v-model="selected" @click.stop="handleInputClick">
-    <div class="vts-tree-container js-vts-tree-container" v-show="showTree" data-tree @click.stop>
+    <div class="vts-tree-container js-vts-tree-container" style="display: none;" v-show="showTree" data-tree @click.stop>
       <ul>
         <item :tree="tree"></item>
       </ul>
@@ -10,91 +10,96 @@
 </template>
 
 <script>
-import EventBus from './EventBus';
-import Item from './Item';
+  import EventBus from './EventBus';
+  import Item from './Item';
 
-export default {
-  name: 'tree-select',
-  props: {
-    name: {},
-    tree: {
-      type: Object,
-      required: true,
+  export default {
+    name: 'tree-select',
+    props: {
+      name: {},
+      tree: {
+        type: Object,
+        required: true,
+      },
+      defaultSelected: {
+      },
+      multiple: {
+        type: Boolean,
+        default: false,
+      },
+      expand: {
+        type: Boolean,
+        default: true,
+      },
     },
-    defaultSelected: {
+    created() {
+      this.attachEvents();
     },
-    multiple: {
-      type: Boolean,
-      default: false,
+    mounted() {
+      document.addEventListener('click', (e) => { if (!e.target.hasAttribute('data-tree')) { this.showTree = false; } });
+      const $input = this.$el.querySelector('.js-vts-input');
+      const $treeContainer = this.$el.querySelector('.js-vts-tree-container');
+      const w = $input.getBoundingClientRect().width - 1;
+      const h = $input.getBoundingClientRect().height;
+      const l = $input.offsetLeft;
+      $treeContainer.style.cssText += `;width: ${w}px; top: ${h}px; left: ${l}px;`;
     },
-    expand: {
-      type: Boolean,
-      default: true,
+    data() {
+      return {
+        selected: [],
+        showTree: false,
+      };
     },
-  },
-  created() {
-    this.attachEvents();
-  },
-  mounted() {
-    document.addEventListener('click', (e) => { if (!e.target.hasAttribute('data-tree')) { this.showTree = false; } });
-    const $input = this.$el.querySelector('.js-vts-input');
-    const $treeContainer = this.$el.querySelector('.js-vts-tree-container');
-    const w = $input.getBoundingClientRect().width - 1;
-    const h = $input.getBoundingClientRect().height;
-    const l = $input.offsetLeft;
-    $treeContainer.setAttribute('style', `width: ${w}px; top: ${h}px; left: ${l}px;`);
-  },
-  data() {
-    return {
-      selected: [],
-      showTree: false,
-    };
-  },
-  methods: {
-    attachEvents() {
-      EventBus.$on('select', this.handleSelect);
+    methods: {
+      attachEvents() {
+        EventBus.$on('select', this.handleSelect);
+      },
+      handleInputClick() {
+        this.showTree = !this.showTree;
+      },
+      handleSelect(selectedOpt) {
+        if (this.multiple) {
+          this.selected.push(selectedOpt.value);
+          this.selected = [...new Set(this.selected)];
+        } else {
+          this.selected = [selectedOpt.value];
+          this.showTree = false;
+        }
+      },
     },
-    handleInputClick() {
-      this.showTree = !this.showTree;
-    },
-    handleSelect(selectedOpt) {
-      if (this.multiple) {
-        this.selected.push(selectedOpt.value);
-        this.selected = [...new Set(this.selected)];
-      } else {
-        this.selected = [selectedOpt.value];
-      }
-    },
-  },
-  computed: {
+    computed: {
 
-  },
-  components: {
-    Item,
-  },
-};
+    },
+    components: {
+      Item,
+    },
+  };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="less" scoped>
-.vts-container {
-  max-width: 300px;
-  max-height: 400px;
-  position: relative;
-}
-ul {
-  list-style-type: none;
-}
-input, .vts-tree-container {
-  border: 1px solid #c4c4c4;
-}
-.vts-tree-container {
-  text-align: left;
-  position: absolute;
-  top: 0;
-  left: 0;
-  & ul {
+  * {
+    margin: 0;
     padding: 0;
   }
-}
+  .vts-container {
+    max-width: 300px;
+    max-height: 400px;
+    position: relative;
+  }
+  ul {
+    list-style-type: none;
+  }
+  input, .vts-tree-container {
+    border: 1px solid #c4c4c4;
+  }
+  .vts-tree-container {
+    text-align: left;
+    position: absolute;
+    top: 0;
+    left: 0;
+    ul {
+      padding: 0;
+    }
+  }
 </style>
